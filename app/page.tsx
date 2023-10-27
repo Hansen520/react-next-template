bde8f6bd159c220c7c3e32e00656544d07bc9aa1
H <744769098@qq.com>
2023-10-27 15:05:40 +0800

    update:next路由

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,39 @@
  * @Date: 2023-08-28 17:11:09
  * @Description: description
  */
-import Image from 'next/image'
-import Link from 'next/link';
+'use client'
+import Link from "next/link";
+import PostList from './blog/PostList'
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const postData = [
+    {
+      id: 1,
+      slug: "aaa",
+      title: "aaa",
+    },
+    {
+      id: 2,
+      slug: "bbb",
+      title: "bbb",
+    },
+    {
+      id: 3,
+      slug: "ccc",
+      title: "ccc",
+    },
+  ];
+
+  const router = useRouter();
   return (
     <main>
-      <h1 className={'text-4xl text-orange-600'}>Page Home!</h1>
+      <h1 className={"text-4xl text-orange-600"}>Page Home!</h1>
+      <br />
       <Link href={"/dashboard"}>Dashboard</Link>
+      <br />
+      <PostList posts={postData} />
+      <button type={"button"} onClick={() => router.push('/dashboard')}>Dashboard</button>
     </main>
-  )
+  );
 }
---
+++ app/{marketing}/blog/page.tsx
@@ -0,0 +1,7 @@
+/*
+ * @Date: 2023-10-27 11:28:00
+ * @Description: description
+ */
+export default function Page() {
+    return <>2222222222222222222</>;
+  }
